44a29bea91acd6308dacd6366f40760896b653e2
dkcoder2023 <136685006+dkcoder2023@users.noreply.github.com>
2023-07-20 10:49:49 +0530

    Update admin.py

--- task project/myblogproject/blogapp/admin.py
+++ task project/myblogproject/blogapp/admin.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
-from blogapp.models import Blog,Comment
+from .models import Blog,Comment
 
 class BlogAdmin(admin.ModelAdmin):
-    class Meta:
-        model=Blog
-        list_display=['title', 'content', 'author', 'created_at']
+    model=Blog
+    list_display=['title', 'content', 'author', 'created_at']
 admin.site.register(Blog,BlogAdmin)
 
 
 class CommentAdmin(admin.ModelAdmin):
-    class Meta:
-        model=Comment
-        list_display=['blog', 'name','email','body','likes','created_at']
+    model=Comment
+    list_display=['blog', 'name','email','body','created_at']
 admin.site.register(Comment,CommentAdmin)
